666fce9cefbcfbabcb782b7623b6119c23db0fb4
Linda <l_m_mull@yahoo.com>
2016-11-20 11:27:38 -0500

    Initial Project commit before any coding done

---
+++ BreweryBucketList/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace BreweryBucketList.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ BreweryBucketList/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BreweryBucketList.Global" Language="C#" %>
